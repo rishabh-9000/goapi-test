4c37971057758a7d5774d203be5abaf30b10f197
Rishabh Dubey <rishabhdubey020@gmail.com>
2019-10-13 16:34:58 +0530

    get and post in single route

--- apis/users.go
+++ apis/users.go
@@ -9,8 +9,8 @@ import (
 	"time"
 )
 
-// CreatePersonEndpoint : Create a new Person
-func CreatePersonEndpoint(w http.ResponseWriter, r *http.Request) {
+// PersonEndpoint : Create a new Person
+func PersonEndpoint(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "application/json")
 
 	var person models.User
@@ -18,7 +18,9 @@ func CreatePersonEndpoint(w http.ResponseWriter, r *http.Request) {
 	_ = json.NewDecoder(r.Body).Decode(&person)
 	collection := config.Client.Database("GoTest").Collection("people")
 	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
-	result, _ := collection.InsertOne(ctx, person)
 
-	json.NewEncoder(w).Encode(result)
+	if r.Method == http.MethodGet {
+		result, _ := collection.InsertOne(ctx, person)
+		json.NewEncoder(w).Encode(result)
+	}
 }
--- main.go
+++ main.go
@@ -17,13 +17,22 @@ func main() {
 
 	router := mux.NewRouter()
 
-	router.HandleFunc("/", Test).Methods("GET")
-	router.HandleFunc("/person", apis.CreatePersonEndpoint).Methods("POST")
+	router.HandleFunc("/",
+		Test).Methods("GET", "POST")
+
+	router.HandleFunc("/api/users",
+		apis.PersonEndpoint).Methods("GET", "POST")
+	// router.HandleFunc("/api/users", apis.CreatePersonEndpoint).Methods("POST")
+	// router.HandleFunc("/api/users", apis.CreatePersonEndpoint).Methods("POST")
+	// router.HandleFunc("/api/users", apis.CreatePersonEndpoint).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":5000", router))
 }
 
 // Test : Endpoint to test API is running
 func Test(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "API Running.")
+	fmt.Fprintf(w, "API Running. And method is: ")
+	if r.Method == http.MethodGet {
+		fmt.Fprintf(w, r.Method)
+	}
 }
