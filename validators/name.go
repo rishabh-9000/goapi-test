ac0da23a244c8cba9e6d535ca5bcbf2b1609c19e
Rishabh Dubey <rishabhdubey020@gmail.com>
2019-10-16 14:21:53 +0530

    added validators

--- validators/name.go
+++ validators/name.go
@@ -1,16 +1,29 @@
 package validators
 
 import (
-	"social-api/models"
+	"regexp"
 )
 
-// NameValidation : Validator for Name
-func NameValidation(name string) models.Err {
-	var response models.Err
+// IsEmpty : Check if string is empty
+func IsEmpty(s string) (bool, string) {
+	re := regexp.MustCompile(`\.+`)
+	return re.Match([]byte(s)), "Input Empty if True."
+}
 
-	if name == "" {
-		response.Message = "Name cannot be empty"
-	}
+// IsChar : Check if string only constains characters
+func IsChar(s string) (bool, string) {
+	re := regexp.MustCompile(`[a-zA-Z]`)
+	return re.Match([]byte(s)), "Input is not just characters."
+}
 
-	return response
+// IsNumeric : Check if string only contains numbers
+func IsNumeric(s string) (bool, string) {
+	re := regexp.MustCompile(`[0-9]`)
+	return re.Match([]byte(s)), "Input is not just numeric."
+}
+
+// IsEmail : Check if string is email
+func IsEmail(s string) (bool, string) {
+	re := regexp.MustCompile(`[a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-Z]+`)
+	return re.Match([]byte(s)), "Input is not an email."
 }
