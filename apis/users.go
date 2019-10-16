cc99a8ec3acd5b95ea4690a7c3c164b66d81dbde
Rishabh Dubey <rishabhdubey020@gmail.com>
2019-10-16 15:39:28 +0530

    updated user.go

--- apis/users.go
+++ apis/users.go
@@ -3,41 +3,67 @@ package apis
 import (
 	"context"
 	"encoding/json"
+	"log"
 	"net/http"
 	"social-api/config"
 	"social-api/models"
 	"social-api/validators"
 	"time"
-
-	"github.com/gorilla/mux"
 )
 
 // UserEndpoint : Create a new User
 func UserEndpoint(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "application/json")
-
-	var user models.User
-
-	_ = json.NewDecoder(r.Body).Decode(&user)
 	collection := config.Client.Database("test").Collection("user")
-	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
 	if r.Method == http.MethodPost {
+		var user models.User
+		var err models.Err
+		var errors []models.Err
+
+		_ = json.NewDecoder(r.Body).Decode(&user)
+
 		// Validation
-		valid := true
-		vars := mux.Vars(r)
+		name := user.Name
+		if !validators.IsEmpty(name) {
+			err.Message = "Name is Empty."
+			errors = append(errors, err)
+		}
+		if !validators.IsChar(name) {
+			err.Message = "Invalid Characters in Name."
+			errors = append(errors, err)
+		}
 
-		name := vars["name"]
+		email := user.Email
+		if !validators.IsEmail(email) {
+			err.Message = "Invalid Email."
+			errors = append(errors, err)
+		}
 
-		valid = validators.IsEmpty(name) && validators.IsChar(name)
+		password := user.Password
+		if !validators.IsEmpty(password) {
+			err.Message = "Password cannot be empty."
+			errors = append(errors, err)
+		}
 
 		// See if user exists
 
 		// Encrypt Password
 
 		// Return JWT
-
-		// result, _ := collection.InsertOne(ctx, user)
-		// json.NewEncoder(w).Encode(result)
+		log.Println(user)
+		if len(errors) != 0 {
+			result, e := collection.InsertOne(ctx, user)
+			if e != nil {
+				log.Fatal(e)
+				json.NewEncoder(w).Encode(e)
+			}
+			json.NewEncoder(w).Encode(result)
+		} else {
+			result := "Internal Server Error"
+			json.NewEncoder(w).Encode(result)
+		}
 	}
 }
